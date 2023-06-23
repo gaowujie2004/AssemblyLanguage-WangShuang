b41f9e1f256e40d7bc89472f84911aea13a6a7c2
gaowujie2004 <gaowujie.2004@bytedance.com>
2023-06-23 13:00:02 +0800

    docs: 注释

--- 8/8-1.asm
+++ 8/8-1.asm
@@ -1,9 +1,12 @@
+; 任务1：用div计算data段中第一个数据除以第二个数据后的结果，商存在第三个数据的存储单元中。
+
+
 assume cs:code, ds:data
 
 data segment
     dd 100001 ; 1_86a1H，  4byte
     dw 100    ; 0064H，    2byte
-    dw 0
+    dw 0      ; 0000H,     2byte
 data ends
 
 code segment
