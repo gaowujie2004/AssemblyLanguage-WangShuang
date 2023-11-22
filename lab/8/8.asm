5bce3eb1dcf806974fcf3b8960b34288240c5bf3
gaowujie <gaowujie2019@gmail.com>
2023-11-22 14:35:01 +0800

    dos: 完善注释

--- lab/8/8.asm
+++ lab/8/8.asm
@@ -11,9 +11,13 @@ codesg segment
     mov di, offset s 
     mov si, offset s2 
     mov ax, cs:[si]
-    mov cs:[di], ax
+    mov cs:[di], ax;  mov cs:[offset s], cs:[offset s2]
 
+    ;若不仔细分析，就会认为一直在这里循环。
+    ;标号s处的机器码，被动态修改了，s标号处的代码被修改成了 s2 标号处的代码。
+    ;实际执行的是 开头处 的代码。
     s0: jmp short s 
+
     
     s1: mov ax,0
     int 21h
